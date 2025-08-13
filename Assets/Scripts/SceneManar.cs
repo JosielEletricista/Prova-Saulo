3a3a61ab46e2d6e92a7cee34e4c254b9c842d484
JosielEletricista <carlos.filho1@escolar.ifrn.edu.br>
2025-08-13 16:17:43 -0300

    Finger mission

---
+++ Assets/Scripts/SceneManar.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneManageue : MonoBehaviour
+{
+    public void play()
+    {
+        SceneManager.LoadScene ("Jogo");
+    }
+
+}
